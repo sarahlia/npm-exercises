9ba40a6a4d2065e618128297765356e268b63716
Sarah L Nainggolan <sarah.l.nainggolan@gmail.com>
2020-04-24 17:16:28 -0500

    finish exercise using export default and import

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 const $ = require('jquery');
 
+//using export default & import
+import mixed from './say-hello';
+
+
 const sayHello = () => console.log('hello');
 sayHello();
 
 $(document).ready( () => {
     $('body').html("Should see this on the page.");
+
+    console.log(mixed.multiply(5, 3));
+    console.log(mixed.add(4, 7));
+    console.log(mixed.msg());
 });
 
---
+++ src/say-hello.js
@@ -0,0 +1,10 @@
+//using export default & import
+const multiply = (x, y) => x + y;
+const add = (x, y) => x + y;
+const msg = () => ("String with a nice message.");
+
+
+export default {multiply, add, msg};
+
+
+
