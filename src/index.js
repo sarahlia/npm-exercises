395195dc0ba87ae6b1885068f97883b6bd4198d5
Sarah L Nainggolan <sarah.l.nainggolan@gmail.com>
2020-04-24 17:29:44 -0500

    finish exercises using module.exports and require

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 const $ = require('jquery');
 
-//using export default & import
-import mixed from './say-hello';
+// using module.exports & require
+const info = require('./say-hello');
 
+//using export default & import
+// import mixed from './say-hello';
+
+console.log(info.whichSideOfTheForce);
+console.log(info.goat);
+console.log(info.someNumber);
 
 const sayHello = () => console.log('hello');
 sayHello();
@@ -10,8 +16,10 @@ sayHello();
 $(document).ready( () => {
     $('body').html("Should see this on the page.");
 
-    console.log(mixed.multiply(5, 3));
-    console.log(mixed.add(4, 7));
-    console.log(mixed.msg());
+    // console.log(mixed.multiply(5, 3));
+    // console.log(mixed.add(4, 7));
+    // console.log(mixed.msg());
+
+
 });
 
--- src/say-hello.js
+++ src/say-hello.js
@@ -1,10 +1,17 @@
 //using export default & import
-const multiply = (x, y) => x + y;
-const add = (x, y) => x + y;
-const msg = () => ("String with a nice message.");
+// const multiply = (x, y) => x + y;
+// const add = (x, y) => x + y;
+// const msg = () => ("String with a nice message.");
+//
+//
+// export default {multiply, add, msg};
 
-
-export default {multiply, add, msg};
-
+// using module.exports & require
+const info = {
+    someNumber: 23,
+    whichSideOfTheForce: "Sith",
+    goat: "Jordan"
+}
+module.exports = info;
 
 
