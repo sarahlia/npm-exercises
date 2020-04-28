c94f58540bb35eeaa1ddfcc38f374e3486c8abcb
Sarah L Nainggolan <sarah.l.nainggolan@gmail.com>
2020-04-27 23:10:07 -0500

    finish npm modules scratch exercise

--- src/index.js
+++ src/index.js
@@ -1,25 +1,29 @@
 const $ = require('jquery');
 
 // using module.exports & require
-const info = require('./say-hello');
+// const info = require('./say-hello');
 
 //using export default & import
-// import mixed from './say-hello';
+import mixed from './say-hello';
 
-console.log(info.whichSideOfTheForce);
-console.log(info.goat);
-console.log(info.someNumber);
+// import {wow, meh} from './say-hello';
 
-const sayHello = () => console.log('hello');
+const sayHello = () => console.log('hello there');
 sayHello();
 
 $(document).ready( () => {
-    $('body').html("Should see this on the page.");
+    $('body').html("Should be on our page.");
 
-    // console.log(mixed.multiply(5, 3));
-    // console.log(mixed.add(4, 7));
-    // console.log(mixed.msg());
+    // console.log(info.whichSideOfTheForce);
+    // console.log(info.goat);
+    // console.log(info.someNumber);
 
+    console.log(mixed.multiply(5, 3));
+    console.log(mixed.add(4, 7));
+    console.log(mixed.msg());
+
+    // console.log(wow(7, 6));
+    // console.log(meh(7, 6));
 
 });
 
--- src/say-hello.js
+++ src/say-hello.js
@@ -1,17 +1,21 @@
 //using export default & import
-// const multiply = (x, y) => x + y;
-// const add = (x, y) => x + y;
-// const msg = () => ("String with a nice message.");
-//
-//
-// export default {multiply, add, msg};
+const multiply = (x, y) => x * y;
+const add = (x, y) => x + y;
+const msg = () => ("String with a nice message.");
+
+
+export default {multiply, add, msg};
 
 // using module.exports & require
-const info = {
-    someNumber: 23,
-    whichSideOfTheForce: "Sith",
-    goat: "Jordan"
-}
-module.exports = info;
+// const info = {
+//     someNumber: 23,
+//     whichSideOfTheForce: "Jedi",
+//     goat: "Jordan"
+// }
+// module.exports = info;
+
+
+// export const wow = (x, y) => x - y;
+// export const meh = (x, y) => x + y;
 
 
